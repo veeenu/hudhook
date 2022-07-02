e593219b65dcdd37f2f0499fd763d19303275b7d
Andrea Venuta <venutawebdesign@gmail.com>
2022-07-02 09:17:59 +0200

    Fixed example

--- imgui-dx11/src/state_backup.rs
+++ imgui-dx11/src/state_backup.rs
@@ -1,3 +1,4 @@
+use log::trace;
 use windows::Win32::Foundation::RECT;
 use windows::Win32::Graphics::Direct3D::D3D_PRIMITIVE_TOPOLOGY;
 use windows::Win32::Graphics::Direct3D11::{
@@ -21,13 +22,13 @@ pub struct StateBackup {
     stencil_ref: u32,
     depth_stencil_state: Option<ID3D11DepthStencilState>,
     ps_shader_resource: [Option<ID3D11ShaderResourceView>; 1],
-    ps_sampler: Option<ID3D11SamplerState>,
+    _ps_sampler: Option<ID3D11SamplerState>,
     pixel_shader: Option<ID3D11PixelShader>,
     vertex_shader: Option<ID3D11VertexShader>,
     ps_instances_count: u32,
     vs_instances_count: u32,
-    ps_instances: Vec<Option<ID3D11ClassInstance>>,
-    vs_instances: Vec<Option<ID3D11ClassInstance>>,
+    ps_instances: Option<ID3D11ClassInstance>,
+    vs_instances: Option<ID3D11ClassInstance>,
     primitive_topology: D3D_PRIMITIVE_TOPOLOGY,
     index_buffer: Option<ID3D11Buffer>,
     vertex_buffer: Option<ID3D11Buffer>,
@@ -42,8 +43,8 @@ pub struct StateBackup {
 impl Default for StateBackup {
     fn default() -> Self {
         Self {
-            ps_instances: vec![None; 256],
-            vs_instances: vec![None; 256],
+            ps_instances: Default::default(),
+            vs_instances: Default::default(),
             scissor_rects_count: Default::default(),
             viewports_count: Default::default(),
             scissor_rects: Default::default(),
@@ -55,7 +56,7 @@ impl Default for StateBackup {
             stencil_ref: Default::default(),
             depth_stencil_state: Default::default(),
             ps_shader_resource: Default::default(),
-            ps_sampler: Default::default(),
+            _ps_sampler: Default::default(),
             pixel_shader: Default::default(),
             vertex_shader: Default::default(),
             ps_instances_count: Default::default(),
@@ -92,10 +93,10 @@ impl StateBackup {
             ctx.PSGetShaderResources(0, &mut r.ps_shader_resource);
             r.ps_instances_count = 256;
             r.vs_instances_count = 256;
-            ctx.PSGetShader(&mut r.pixel_shader, &mut r.ps_instances[0], &mut r.ps_instances_count);
+            ctx.PSGetShader(&mut r.pixel_shader, &mut r.ps_instances as *mut _, &mut r.ps_instances_count);
             ctx.VSGetShader(
                 &mut r.vertex_shader,
-                &mut r.vs_instances[0],
+                &mut r.vs_instances,
                 &mut r.vs_instances_count,
             );
             ctx.VSGetConstantBuffers(0, &mut r.vertex_constant_buffer);
@@ -123,50 +124,22 @@ impl StateBackup {
             ctx.RSSetScissorRects(&self.scissor_rects);
             ctx.RSSetViewports(&self.viewports);
             ctx.RSSetState(self.rasterizer_state);
-            // self.rasterizer_state
-            //     .as_ref()
-            //     .map(|r| r.Release())
-            //     .unwrap_or(0);
             ctx.OMSetBlendState(self.blend_state, &self.blend_factor as _, self.sample_mask);
-            // self.blend_state.as_ref().map(|r| r.Release()).unwrap_or(0);
             ctx.OMSetDepthStencilState(self.depth_stencil_state, self.stencil_ref);
-            // self.depth_stencil_state
-            //     .as_ref()
-            //     .map(|r| r.Release())
-            //     .unwrap_or(0);
             ctx.PSSetShaderResources(0, &self.ps_shader_resource);
-            // self.ps_shader_resource
-            //     .as_ref()
-            //     .map(|r| r.Release())
-            //     .unwrap_or(0);
-            ctx.PSSetSamplers(0, &[self.ps_sampler]);
-            // self.ps_sampler.as_ref().map(|r| r.Release()).unwrap_or(0);
-            ctx.PSSetShader(self.pixel_shader, &self.ps_instances);
-            // self.pixel_shader.as_ref().map(|r| r.Release()).unwrap_or(0);
-            // for i in 0..self.ps_instances_count {
-            //     self.ps_instances[i as usize]
-            //         .as_ref()
-            //         .map(|r| r.Release())
-            //         .unwrap_or(0);
-            // }
-            ctx.VSSetShader(self.vertex_shader, &self.vs_instances);
-            // self.vertex_shader
-            //     .as_ref()
-            //     .map(|r| r.Release())
-            //     .unwrap_or(0);
-            // for i in 0..self.vs_instances_count {
-            //     self.vs_instances[i as usize]
-            //         .as_ref()
-            //         .map(|r| r.Release())
-            //         .unwrap_or(0);
-            // }
+            // ctx.PSSetSamplers(0, &[self.ps_sampler]);
+            if self.ps_instances.is_some() {
+                ctx.PSSetShader(self.pixel_shader, &[self.ps_instances]);
+            }
+            if self.vs_instances.is_some() {
+                ctx.VSSetShader(self.vertex_shader, &[self.vs_instances]);
+            }
             ctx.IASetPrimitiveTopology(self.primitive_topology);
             ctx.IASetIndexBuffer(
                 self.index_buffer,
                 self.index_buffer_format,
                 self.index_buffer_offset,
             );
-            // self.index_buffer.as_ref().map(|r| r.Release()).unwrap_or(0);
             ctx.IASetVertexBuffers(
                 0,
                 1,
@@ -174,12 +147,7 @@ impl StateBackup {
                 &self.vertex_buffer_stride,
                 &self.vertex_buffer_offset,
             );
-            // self.vertex_buffer
-            //     .as_ref()
-            //     .map(|r| r.Release())
-            //     .unwrap_or(0);
             ctx.IASetInputLayout(self.input_layout);
-            // self.input_layout.as_ref().map(|r| r.Release()).unwrap_or(0);
         }
     }
 }
