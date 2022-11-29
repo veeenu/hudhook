5d1f14707c72ae7856c7e383e9b27b250b87c67e
Jakobzs <31919330+Jakobzs@users.noreply.github.com>
2022-11-29 22:22:01 +0100

    Prepared files for opengl3 integration test

--- tests/harness/mod.rs
+++ tests/harness/mod.rs
@@ -1,2 +1,3 @@
 pub mod dx11;
 pub mod dx12;
+pub mod opengl3;
---
+++ tests/opengl3.rs
@@ -0,0 +1,56 @@
+mod harness;
+
+use std::thread;
+use std::time::Duration;
+
+use harness::opengl3::Opengl3Harness;
+use hudhook::hooks::dx12::ImguiDx12Hooks;
+use hudhook::hooks::{self, ImguiRenderLoop, ImguiRenderLoopFlags};
+use imgui::{Condition, Window};
+use simplelog::*;
+
+#[test]
+fn test_imgui_opengl3() {
+    struct Opengl3HookExample;
+
+    impl Opengl3HookExample {
+        fn new() -> Self {
+            println!("Initializing");
+            hudhook::utils::alloc_console();
+
+            Opengl3HookExample
+        }
+    }
+
+    impl ImguiRenderLoop for Opengl3HookExample {
+        fn render(&mut self, ui: &mut imgui::Ui, _: &ImguiRenderLoopFlags) {
+            Window::new("Hello world").size([300.0, 300.0], Condition::FirstUseEver).build(
+                ui,
+                || {
+                    ui.text("Hello world!");
+                    ui.text("こんにちは世界！");
+                    ui.text("This...is...imgui-rs!");
+                    ui.separator();
+                    let mouse_pos = ui.io().mouse_pos;
+                    ui.text(format!("Mouse Position: ({:.1},{:.1})", mouse_pos[0], mouse_pos[1]));
+                },
+            );
+        }
+    }
+
+    TermLogger::init(LevelFilter::Trace, Config::default(), TerminalMode::Mixed, ColorChoice::Auto)
+        .ok();
+
+    let dx12_harness = Opengl3Harness::new("Opengl3 hook example");
+    thread::sleep(Duration::from_millis(500));
+
+    unsafe {
+        let hooks: Box<dyn hooks::Hooks> =
+            { Opengl3HookExample::new().into_hook::<ImguiDx12Hooks>() };
+        hooks.hook();
+        hudhook::lifecycle::global_state::set_hooks(hooks);
+    }
+
+    thread::sleep(Duration::from_millis(5000));
+    drop(dx12_harness);
+}
