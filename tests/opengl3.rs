b41bf4a25f381f2f24baa313fd22f65abebc2417
Jakobzs <31919330+Jakobzs@users.noreply.github.com>
2023-01-04 13:03:17 +0100

    Fixed build

--- tests/opengl3.rs
+++ tests/opengl3.rs
@@ -24,17 +24,14 @@ fn test_imgui_opengl3() {
 
     impl ImguiRenderLoop for Opengl3HookExample {
         fn render(&mut self, ui: &mut imgui::Ui, _: &ImguiRenderLoopFlags) {
-            Window::new("Hello world").size([300.0, 300.0], Condition::FirstUseEver).build(
-                ui,
-                || {
-                    ui.text("Hello world!");
-                    ui.text("こんにちは世界！");
-                    ui.text("This...is...imgui-rs!");
-                    ui.separator();
-                    let mouse_pos = ui.io().mouse_pos;
-                    ui.text(format!("Mouse Position: ({:.1},{:.1})", mouse_pos[0], mouse_pos[1]));
-                },
-            );
+            ui.window("Hello world").size([300.0, 300.0], Condition::FirstUseEver).build(|| {
+                ui.text("Hello world!");
+                ui.text("こんにちは世界！");
+                ui.text("This...is...imgui-rs!");
+                ui.separator();
+                let mouse_pos = ui.io().mouse_pos;
+                ui.text(format!("Mouse Position: ({:.1},{:.1})", mouse_pos[0], mouse_pos[1]));
+            });
         }
     }
 
