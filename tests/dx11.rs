80ecc039d18dc33cd314325366e1296387da0c0f
Andrea Venuta <venutawebdesign@gmail.com>
2023-02-07 12:50:33 +0100

    Remap shift/ctrl/menu virtual keys (#77)

--- tests/dx11.rs
+++ tests/dx11.rs
@@ -6,7 +6,7 @@ use std::time::Duration;
 use harness::dx11::Dx11Harness;
 use hudhook::hooks::dx11::ImguiDx11Hooks;
 use hudhook::hooks::{self, ImguiRenderLoop, ImguiRenderLoopFlags};
-use imgui::Condition;
+use imgui::{Condition, StyleColor};
 use simplelog::*;
 
 #[test]
@@ -28,6 +28,25 @@ fn test_imgui_dx11() {
                 ui.text("Hello world!");
                 ui.text("こんにちは世界！");
                 ui.text("This...is...imgui-rs!");
+                for y in 0..16 {
+                    for x in 0..16 {
+                        let btn = y * 16 + x;
+                        let _token = ui.push_style_color(
+                            StyleColor::Text,
+                            if ui.io().keys_down[btn as usize] {
+                                [0., 1., 0., 1.]
+                            } else {
+                                [1., 1., 1., 1.]
+                            },
+                        );
+                        ui.text(format!("{btn:02x}"));
+                        ui.same_line();
+                    }
+                    ui.new_line();
+                }
+                ui.text(if ui.io().key_shift { "SHIFT" } else { "shift" });
+                ui.text(if ui.io().key_ctrl { "CTRL" } else { "ctrl" });
+                ui.text(if ui.io().key_alt { "ALT" } else { "alt" });
                 ui.separator();
                 let mouse_pos = ui.io().mouse_pos;
                 ui.text(format!("Mouse Position: ({:.1},{:.1})", mouse_pos[0], mouse_pos[1]));
