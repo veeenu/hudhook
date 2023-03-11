46539aeac9579a3a7205d96d088bf79286bb51ea
Jakob <31919330+Jakobzs@users.noreply.github.com>
2023-03-11 18:40:16 +0100

    More testing with tracing

--- examples/opengl3_hook.rs
+++ examples/opengl3_hook.rs
@@ -37,12 +37,16 @@ use tracing::trace;
 pub unsafe extern "stdcall" fn DllMain(hmodule: HINSTANCE, reason: u32, _: *mut std::ffi::c_void) {
     if reason == DLL_PROCESS_ATTACH {
         hudhook::lifecycle::global_state::set_module(hmodule);
-        let (non_blocking, _guard) = tracing_appender::non_blocking(std::io::stdout());
-        tracing_subscriber::fmt().with_writer(non_blocking).init();
 
-        trace!("DllMain()");
+        // trace!("DllMain()");
         std::thread::spawn(move || {
             let hooks: Box<dyn hooks::Hooks> = { HookYou::new().into_hook::<ImguiOpenGl3Hooks>() };
+
+            let (non_blocking, _guard) = tracing_appender::non_blocking(std::io::stdout());
+            tracing_subscriber::fmt().with_writer(non_blocking).init();
+
+            trace!("Testy");
+
             hooks.hook();
             hudhook::lifecycle::global_state::set_hooks(hooks);
         });
