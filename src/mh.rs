7aec187830a525ce14eff237a9d5cf29a092881c
vSylva <58687743+vSylva@users.noreply.github.com>
2023-06-14 18:44:36 +0800

    Remove local minhook
    Perhaps this is a better way
    
    Replace local minhook with minhook_raw = "0.1.3"

--- src/mh.rs
+++ src/mh.rs
@@ -1,72 +1,11 @@
-#![allow(dead_code, non_snake_case, non_camel_case_types)]
+#![allow(dead_code)]
 
 use std::ffi::c_void;
 use std::ptr::null_mut;
 use std::sync::LazyLock;
 
+use minhook_raw::*;
 use tracing::debug;
-
-#[allow(non_camel_case_types)]
-#[must_use]
-#[repr(C)]
-#[derive(Debug, Clone, Copy, PartialEq, Eq)]
-pub enum MH_STATUS {
-    /// Unknown error. Should not be returned.
-    MH_UNKNOWN = -1,
-    /// Successful.
-    MH_OK = 0,
-    /// MinHook is already initialized.
-    MH_ERROR_ALREADY_INITIALIZED,
-    /// MinHook is not initialized yet, or already uninitialized.
-    MH_ERROR_NOT_INITIALIZED,
-    /// The hook for the specified target function is already created.
-    MH_ERROR_ALREADY_CREATED,
-    /// The hook for the specified target function is not created yet.
-    MH_ERROR_NOT_CREATED,
-    /// The hook for the specified target function is already enabled.
-    MH_ERROR_ENABLED,
-    /// The hook for the specified target function is not enabled yet, or
-    /// already disabled.
-    MH_ERROR_DISABLED,
-    /// The specified pointer is invalid. It points the address of non-allocated
-    /// and/or non-executable region.
-    MH_ERROR_NOT_EXECUTABLE,
-    /// The specified target function cannot be hooked.
-    MH_ERROR_UNSUPPORTED_FUNCTION,
-    /// Failed to allocate memory.
-    MH_ERROR_MEMORY_ALLOC,
-    /// Failed to change the memory protection.
-    MH_ERROR_MEMORY_PROTECT,
-    /// The specified module is not loaded.
-    MH_ERROR_MODULE_NOT_FOUND,
-    /// The specified function is not found.
-    MH_ERROR_FUNCTION_NOT_FOUND,
-}
-
-extern "system" {
-    pub fn MH_Initialize() -> MH_STATUS;
-    pub fn MH_Uninitialize() -> MH_STATUS;
-    pub fn MH_CreateHook(
-        pTarget: *mut c_void,
-        pDetour: *mut c_void,
-        ppOriginal: *mut *mut c_void,
-    ) -> MH_STATUS;
-    pub fn MH_EnableHook(pTarget: *mut c_void) -> MH_STATUS;
-    pub fn MH_QueueEnableHook(pTarget: *mut c_void) -> MH_STATUS;
-    pub fn MH_DisableHook(pTarget: *mut c_void) -> MH_STATUS;
-    pub fn MH_QueueDisableHook(pTarget: *mut c_void) -> MH_STATUS;
-    pub fn MH_ApplyQueued() -> MH_STATUS;
-}
-
-impl MH_STATUS {
-    pub fn ok(self) -> Result<(), MH_STATUS> {
-        if self == MH_STATUS::MH_OK {
-            Ok(())
-        } else {
-            Err(self)
-        }
-    }
-}
 
 /// Structure that holds original address, hook function address, and trampoline
 /// address for a given hook.
@@ -80,10 +19,11 @@ impl MhHook {
     /// # Safety
     pub unsafe fn new(addr: *mut c_void, hook_impl: *mut c_void) -> Result<Self, MH_STATUS> {
         static INIT_CELL: LazyLock<()> = LazyLock::new(|| {
-            let status = unsafe { crate::mh::MH_Initialize() };
+            let status = unsafe { MH_Initialize() };
             debug!("MH_Initialize: {:?}", status);
-
-            status.ok().expect("Couldn't initialize hooks");
+            if status != MH_STATUS::MH_OK {
+                panic!("Couldn't initialize hooks");
+            }
         });
 
         LazyLock::force(&INIT_CELL);
@@ -92,7 +32,9 @@ impl MhHook {
         let status = MH_CreateHook(addr, hook_impl, &mut trampoline);
         debug!("MH_CreateHook: {:?}", status);
 
-        status.ok()?;
+        if status != MH_STATUS::MH_OK {
+            panic!("Couldn't create hook")
+        }
 
         Ok(Self { addr, hook_impl, trampoline })
     }
