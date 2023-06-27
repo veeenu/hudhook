15231253b5959cfff32f56fdcfe4679f9c84c772
Andrea Venuta <venutawebdesign@gmail.com>
2023-06-27 11:09:41 +0200

    Gate renderers behind feature flags (#113)

--- src/renderers/mod.rs
+++ src/renderers/mod.rs
@@ -1,6 +1,10 @@
 //! Engine-specific renderers.
 
+#[cfg(feature = "dx11")]
 pub mod imgui_dx11;
+#[cfg(feature = "dx12")]
 pub mod imgui_dx12;
+#[cfg(feature = "dx9")]
 pub mod imgui_dx9;
+#[cfg(feature = "opengl3")]
 pub mod imgui_opengl3;
